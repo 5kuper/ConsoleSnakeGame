0580bb27e750eee1e0f57d28aceb063d55c0c045
Dmitry Polyukhovich <5kuper@proton.me>
2022-10-23 18:08:38 +0300

    Refactor IntVector2

--- src/ConsoleSnakeGame/Utilities/IntVector2.cs
+++ src/ConsoleSnakeGame/Utilities/IntVector2.cs
@@ -2,10 +2,9 @@
 
 namespace Utilities.Numerics;
 
-internal struct IntVector2 : IEquatable<IntVector2>
+internal record struct IntVector2(int X, int Y)
 {
     public IntVector2(int value) : this(value, value) { }
-    public IntVector2(int x, int y) => (X, Y) = (x, y);
 
     public static IntVector2 Zero => default;
     public static IntVector2 One => new(1);
@@ -15,9 +14,6 @@ internal struct IntVector2 : IEquatable<IntVector2>
 
     public static IntVector2 Left => new(-1, 0);
     public static IntVector2 Right => new(1, 0);
-
-    public int X { get; set; }
-    public int Y { get; set; }
 
     public float Lenght => MathF.Sqrt(LenghtSquared);
     public int LenghtSquared => Dot(this, this);
@@ -44,16 +40,6 @@ internal struct IntVector2 : IEquatable<IntVector2>
         return (a.X * b.X) + (a.Y * b.Y);
     }
 
-    public bool Equals(IntVector2 other)
-    {
-        return X.Equals(other.X) && Y.Equals(other.Y);
-    }
-
-    public override bool Equals(object? obj)
-    {
-        return obj is IntVector2 vector && Equals(vector);
-    }
-
     public override int GetHashCode() => HashCode.Combine(X, Y);
 
     public static IntVector2 operator -(IntVector2 value) => Zero - value;
@@ -66,9 +52,6 @@ internal struct IntVector2 : IEquatable<IntVector2>
     public static IntVector2 operator *(IntVector2 vec, int num) => new IntVector2(num) * vec;
     public static IntVector2 operator *(int num, IntVector2 vec) => new IntVector2(num) * vec;
     public static IntVector2 operator /(IntVector2 vec, int num) => new IntVector2(num) / vec;
-
-    public static bool operator ==(IntVector2 a, IntVector2 b) => a.X == b.X && a.Y == b.Y;
-    public static bool operator !=(IntVector2 a, IntVector2 b) => !(a == b);
 
     public static implicit operator Vector2(IntVector2 value) => new(value.X, value.Y);
     public static explicit operator IntVector2(Vector2 value) => new((int)value.X, (int)value.Y);
